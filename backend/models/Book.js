82491bb3bf74ba0c8a7c4fb78433262a6c043de6
Saad Sayyed <saadsyed950@gmail.com>
2025-10-17 17:47:33 +0530

    Book model

---
+++ backend/models/Book.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+export default Book;
